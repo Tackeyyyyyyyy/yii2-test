bdba265b2234d12746f975b65bd6a6093f5d3b05
Tackeyyyyyyyy <meteoreon@yahoo.co.jp>
2019-12-19 21:21:43 +0900

    modify test data

--- test_dada.sql
+++ test_dada.sql
@@ -31,3 +31,20 @@ INSERT INTO names (id, first_name, last_name) VALUES (1, 'test', 'hoge');
 INSERT INTO names (id, first_name, last_name) VALUES (2, 'test2', 'hoge2');
 INSERT INTO names (id, first_name, last_name) VALUES (3, 'test3', 'hoge3');
 INSERT INTO names (id, first_name, last_name) VALUES (4, 'test4', 'hoge4');
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `auth_key` varchar(32) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `password_reset_token` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `status` smallint(10) NOT NULL,
+  `role` int(11) NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO `user` (`id`, `username`, `auth_key`, `password_hash`, `password_reset_token`, `email`, `status`, `role`, `created_at`, `updated_at`) VALUES
+(1, 'admin', '', '$2y$13$uqe3LPW9ya3RZhynJpPN5um9fvdxUmoqjOqQBJDdIDXSKxRZB5bPu', '', '', 10, 0, 0, 0);
